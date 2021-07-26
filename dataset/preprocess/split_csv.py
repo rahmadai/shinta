27d9a84c459690b072a1651589ff60476f6336d1
Rahmad Kurniawan <rahmadkurniawan.ai@gmail.com>
2021-07-26 23:40:24 +0700

    add gitignore and split_csv

---
+++ dataset/preprocess/split_csv.py
@@ -0,0 +1,36 @@
+import os
+import csv
+
+
+def split(filehandler, delimiter=',', row_limit=5000,
+          output_name_template='output_%s.txt', output_path='output_5000', keep_headers=False):
+    reader = csv.reader(filehandler, delimiter=delimiter)
+    current_piece = 1
+    current_out_path = os.path.join(
+        output_path,
+        output_name_template % current_piece
+    )
+    current_out_writer = csv.writer(open(current_out_path, 'w'), delimiter=delimiter)
+    current_limit = row_limit
+    if keep_headers:
+        headers = reader.next()
+        current_out_writer.writerow(headers)
+    for i, row in enumerate(reader):
+        if i + 1 > current_limit:
+            current_piece += 1
+            current_limit = row_limit * current_piece
+            current_out_path = os.path.join(
+                output_path,
+                output_name_template % current_piece
+            )
+            current_out_writer = csv.writer(open(current_out_path, 'w'), delimiter=delimiter)
+            if keep_headers:
+                current_out_writer.writerow(headers)
+        current_out_writer.writerow(row)
+        
+        
+def main():
+    split(open('/home/rahmad/other/Work/Shinta/dataset/flickr_dataset/results.csv', 'r'));
+
+if __name__ == "__main__":
+    main()
